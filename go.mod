b6f65d8eb739148a10865592ac9f58a7eb8db9c1
PM <140794313+pkoyeb@users.noreply.github.com>
2023-08-04 14:14:05 +0200

    chore: init commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/pkoyeb/foo
+
+go 1.20
---
+++ main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+)
+
+func main() {
+
+	for {
+		for port := 9000; port <= 9999; port++ {
+			url := fmt.Sprintf("http://127.0.0.1:%d/clusters", port)
+			fmt.Printf("Scanning port: %d", port)
+			res, err := http.Get(url)
+			if err != nil {
+				fmt.Println(fmt.Sprint("Unnable to reach admin port : %s", err.Error()))
+				continue
+			}
+
+			defer res.Body.Close()
+
+			body, err := io.ReadAll(res.Body)
+
+			if err != nil {
+				fmt.Println(err.Error())
+				continue
+			}
+			fmt.Println(fmt.Sprintf("Response from port %d : %s", port, body))
+
+		}
+	}
+}
